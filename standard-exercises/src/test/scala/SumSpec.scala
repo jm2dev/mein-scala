e3a1e9a4b2a75027b32e836ff6ca3beae7dd8932
José Miguel <jm@0pt1mates.com>
2017-11-28 00:18:37 +0100

    From simple function to Monoid.

--- standard-exercises/src/test/scala/SumSpec.scala
+++ standard-exercises/src/test/scala/SumSpec.scala
@@ -6,9 +6,33 @@ class SumSpec extends FlatSpec with Matchers {
     sum(3 :: 4 :: Nil) should be(7)
   }
 
-  it should "any collection of numbers" in {
+  it should "add any collection of numbers" in {
     sum(3 :: 4 :: 2 :: Nil) should be(9)
   }
 
-  def sum(xs: List[Int]): Int = xs.foldLeft(0)(_ + _)
+  it should "add any collection of strings" in {
+    sum("hola" :: " y " :: "adios" :: Nil) should be("hola y adios")
+  }
+
+  trait Monoid[A] {
+    def mappend(a1: A, a2: A): A
+    def mzero: A
+  }
+
+  object Monoid {
+    implicit val IntMonoid: Monoid[Int] = new Monoid[Int] {
+      def mappend(a: Int, b: Int): Int = a + b
+      def mzero: Int = 0
+    }
+  
+    implicit val StringMonoid: Monoid[String] = new Monoid[String] {
+      def mappend(a: String, b: String): String = a + b
+      def mzero: String = ""
+    }
+  }
+
+  def sum[A: Monoid](xs: List[A]): A = {
+    val m = implicitly[Monoid[A]]
+    xs.foldLeft(m.mzero)(m.mappend)
+  }
 }
